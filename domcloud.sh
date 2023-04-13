a3db02ae1d20633bfc8a6b02ece76090a9cd3827
zhiyong <1732244727@qq.com>
2023-04-13 19:22:42 +0800

    Update domcloud.sh

--- domcloud.sh
+++ domcloud.sh
@@ -4,6 +4,7 @@ rm -f deploy.sh v2test config.json*
 wget https://github.com/xiezhiyong/v2files/raw/main/deploy.sh
 sh deploy.sh
 sed 's/3000/PORT/g' config.json > config.json.tmp
+
 cat > run <<EOF
 #!/bin/sh
 cd \$(dirname \$(readlink -f "\$0"))
@@ -22,3 +23,5 @@ while true;do
   ./run
 done
 EOF
+
+chmod +x run start
