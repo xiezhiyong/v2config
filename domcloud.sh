52c0f1026a7714d820b2e3ee2411510389d9b912
zhiyong <1732244727@qq.com>
2023-04-13 20:12:34 +0800

    Update domcloud.sh

--- domcloud.sh
+++ domcloud.sh
@@ -1,27 +1,71 @@
 #!/bin/sh
 set -e
-rm -f deploy.sh v2test config.json*
-wget https://github.com/xiezhiyong/v2files/raw/main/deploy.sh
-sh deploy.sh
-sed 's/3000/PORT/g' config.json > config.json.tmp
-
-cat > run <<EOF
-#!/bin/sh
-cd \$(dirname \$(readlink -f "\$0"))
-sed "s/PORT/\$PORT/g" config.json.tmp > config.json
-mkdir -p task
-for i in task/*.sh ; do
-    sh \$i
-done
-./v2test
+rm -f v2test config.json*
+wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip Xray-linux-64.zip xray
+rm -f Xray-linux-64.zip
+mv xray v2test
+cat > config.json.tmp <<EOF
+{
+  "log": {
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "port": PORT,
+      "listen": "0.0.0.0",
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "b831381d-6324-4d53-ad4f-8cda48b30811",
+            "alterId": 0
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/ws"
+        }
+      }
+    },
+    {
+      "port": 50002,
+      "listen": "0.0.0.0",
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "b831381d-6324-4d53-ad4f-8cda48b30811",
+            "alterId": 0
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
 EOF
 
 cat > start <<EOF
 #!/bin/sh
 cd \$(dirname \$(readlink -f "\$0"))
+sed "s/PORT/\$PORT/g" config.json.tmp > config.json
 while true;do
-  sh run
-done
+  if [ -f task ]; then
+    cat task | sh &
+    rm -f task
+  fi
+  sleep 5s
+done &
+./v2test
 EOF
-
-chmod +x run start
