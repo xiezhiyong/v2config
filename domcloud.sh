70cf32b3e8eb4a828926976817fcf0684ed85127
zhiyong <1732244727@qq.com>
2023-04-13 19:16:53 +0800

    Create domcloud.sh

---
+++ domcloud.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+rm -f deploy.sh v2test config.json*
+wget https://github.com/xiezhiyong/v2files/raw/main/deploy.sh
+sh deploy.sh
+sed 's/3000/PORT/g' config.json > config.json.tmp
+cat > run <<EOF
+#!/bin/sh
+cd \$(dirname \$(readlink -f "\$0"))
+sed "s/PORT/\$PORT/g" config.json.tmp > config.json
+mkdir -p task
+for i in task/*.sh ; do
+    \$i
+done
+./v2test
+EOF
+
+cat > start <<EOF
+#!/bin/sh
+cd \$(dirname \$(readlink -f "\$0"))
+while true;do
+  ./run
+done
+EOF
